0c074355fc0562296c1d6741dffe650c88a2b769
davidfeng88 <david.feng.ge@gmail.com>
2017-07-08 14:50:42 -0400

    edit webpack config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+const webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/collision_viz.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /\.node$/,
+      loader: "node-loader"
+    }
+    ]
+  }
+};
