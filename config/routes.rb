1ae7b42a2823753485d8d79a08b4364f9d514d6f
davidfeng88 <david.feng.ge@gmail.com>
2017-07-10 11:36:50 -0400

    disable import routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :collisions do
-    collection { post :import }
-  end
+  # for collision data import from csv
+  # root to: 'collisions#index'
+  # resources :collisions do
+  #   collection { post :import }
+  # end
 
   namespace :api, defaults: {format: :json} do
     resources :collisions, only: [:index, :show]
@@ -9,7 +11,6 @@ Rails.application.routes.draw do
 
   root "static_pages#root"
 
-  # for collision data import from csv
-  # root to: 'collisions#index'
+
 
 end
