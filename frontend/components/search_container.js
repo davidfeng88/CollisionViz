a06b89ad797c5615658b79ac7b437af1768a5ed4
davidfeng88 <david.feng.ge@gmail.com>
2017-07-19 17:26:51 -0400

    style highlight

--- frontend/components/search_container.js
+++ frontend/components/search_container.js
@@ -44,23 +44,27 @@ class Search extends React.Component {
             <img src={window.staticImages.logo} />
             <h1>CollisionViz</h1>
           </div>
-          <h3>A visualization of motor vehicle collisions
-          in NYC on 6/22/2017 (Fri.)</h3>
           <h2>by <a href='https://davidfeng.us/' target="_blank">Ge "David" Feng</a></h2>
+          <h3>
+          A visualization of motor vehicle collisions
+          in NYC on 6/22/2017.<br />
+          Click on markers for collision details.</h3>
+
           <div className='links'>
-            <a href='https://github.com/davidfeng88' target="_blank">
-              <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://www.linkedin.com/in/gfeng/' target="_blank">
-              <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://angel.co/ge-david-feng' target="_blank">
-              <i className="fa fa-angellist fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://davidfeng.us/' target="_blank">
-              <i className="fa fa-user fa-2x" aria-hidden="true"></i>
-            </a>
+          <a href='https://github.com/davidfeng88' target="_blank">
+          <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://www.linkedin.com/in/gfeng/' target="_blank">
+          <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://angel.co/ge-david-feng' target="_blank">
+          <i className="fa fa-angellist fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://davidfeng.us/' target="_blank">
+          <i className="fa fa-user fa-2x" aria-hidden="true"></i>
+          </a>
           </div>
+
 
           <FilterForm
             filters={filters}
@@ -71,10 +75,10 @@ class Search extends React.Component {
 
         </aside>
 
-        <Map collisions={collisions} updateHighlight={updateHighlight}
-          taxi={taxi} bike={bike} motorcycle={motorcycle} />
+          <Map collisions={collisions} updateHighlight={updateHighlight}
+            taxi={taxi} bike={bike} motorcycle={motorcycle} />
         </div>
-        
+
         <HighlightContainer />
 
       </div>
