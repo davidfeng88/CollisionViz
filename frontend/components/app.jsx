59c502da6b5ecef8f324cf82a949856d99d7e026
davidfeng88 <david.feng.ge@gmail.com>
2017-07-20 12:06:47 -0400

    remove extra div in app js

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,38 +6,36 @@ import HighlightContainer from './highlight_container';
 
 const App = () => {
   return (
-    <div className='page-container'>
-      <div className='container'>
-        <div className='main'>
-          <aside>
-            <div className='logo'>
-              <img src={window.staticImages.logo} />
-              <h1>CollisionViz</h1>
-            </div>
-            <h3>
-            A visualization of motor vehicle collisions
-            in NYC on 6/22/2017.<br />
-            Click on markers for collision details.</h3>
-            <div className='links'>
-            <a href='https://github.com/davidfeng88' target="_blank">
-            <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://www.linkedin.com/in/gfeng/' target="_blank">
-            <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://angel.co/ge-david-feng' target="_blank">
-            <i className="fa fa-angellist fa-2x" aria-hidden="true"></i>
-            </a>
-            <a href='https://davidfeng.us/' target="_blank">
-            <i className="fa fa-user fa-2x" aria-hidden="true"></i>
-            </a>
-            </div>
-            <FilterFormContainer />
-          </aside>
-          <MapContainer />
-        </div>
-        <HighlightContainer />
+    <div className='container'>
+      <div className='main'>
+        <aside>
+          <div className='logo'>
+            <img src={window.staticImages.logo} />
+            <h1>CollisionViz</h1>
+          </div>
+          <h3>
+          A visualization of motor vehicle collisions
+          in NYC on 6/22/2017.<br />
+          Click on markers for collision details.</h3>
+          <div className='links'>
+          <a href='https://github.com/davidfeng88' target="_blank">
+          <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://www.linkedin.com/in/gfeng/' target="_blank">
+          <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://angel.co/ge-david-feng' target="_blank">
+          <i className="fa fa-angellist fa-2x" aria-hidden="true"></i>
+          </a>
+          <a href='https://davidfeng.us/' target="_blank">
+          <i className="fa fa-user fa-2x" aria-hidden="true"></i>
+          </a>
+          </div>
+          <FilterFormContainer />
+        </aside>
+        <MapContainer />
       </div>
+      <HighlightContainer />
     </div>
   );
 };
