caa996eaf9532a55ac60b3cc072bfb1436bd6614
davidfeng88 <david.feng.ge@gmail.com>
2017-07-29 19:00:31 -0400

    create map folder

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './header/header';
 import ControlPanelContainer from './control_panel_container';
-import MapContainer from './map_container';
+import MapContainer from './map/map_container';
 import HighlightContainer from './highlight_container';
 
 const App = () => {
@@ -17,31 +17,3 @@ const App = () => {
 };
 
 export default App;
-
-// about CollisionViz
-// <h3>
-// A visualization of motor vehicle collisions
-// in NYC on 6/22/2017.<br />
-// Click on markers for collision details.</h3>
-// <div className='github-repo'>
-// <a href='https://github.com/davidfeng88/CollisionViz' target="_blank">
-// GitHub Repo
-// </a>
-// </div>
-
-// about me
-// <div className='links'>
-// <a href='https://davidfeng.us/' target="_blank">
-// <i className="fa fa-user fa-lg" aria-hidden="true"></i>
-// </a>
-// <a href='https://github.com/davidfeng88' target="_blank">
-// <i className="fa fa-github fa-lg" aria-hidden="true"></i>
-// </a>
-// <a href='https://www.linkedin.com/in/gfeng/' target="_blank">
-// <i className="fa fa-linkedin-square fa-lg" aria-hidden="true"></i>
-// </a>
-// <a href='https://angel.co/ge-david-feng' target="_blank">
-// <i className="fa fa-angellist fa-lg" aria-hidden="true"></i>
-// </a>
-//
-// </div>
