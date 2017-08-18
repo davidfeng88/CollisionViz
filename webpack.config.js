41d05b2ec9e96c5718a4e0e751a0900da832b6a7
davidfeng88 <david.feng.ge@gmail.com>
2017-08-18 09:53:10 -0400

    serve bundle js map production

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,12 @@ const prodPlugins = [
       'NODE_ENV': JSON.stringify('production')
     }
   }),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  }),
-  new webpack.optimize.AggressiveMergingPlugin(),
+  new webpack.optimize.UglifyJsPlugin(),
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: true
+  //   }
+  // }),
 ];
 
 plugins = plugins.concat(
