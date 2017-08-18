46bbaf9272b8004d54c69bcb0081e8d9f6535024
davidfeng88 <david.feng.ge@gmail.com>
2017-08-18 15:00:45 -0400

    update instruction modal

--- lib/javascript/components/header/about_collision_viz.jsx
+++ lib/javascript/components/header/about_collision_viz.jsx
@@ -6,10 +6,13 @@ const AboutCollisionViz = props => (
   <ModalTemplate {...props}>
     <h3>About CollisionViz</h3>
     <p className="modal-row">
-      A visualization of motor vehicle collisions in NYC.
+      CollisionViz is a data visualization web app for motor vehicle collisions in New York City.
     </p>
     <p className="modal-row">
-      Click on map icons for collision details.
+      1. Select the date and the start time of the visualization.
+    </p>
+    <p className="modal-row">
+      2. Start/pause the visualization. Click on markers for collision details.
     </p>
     <div className='modal-link-div'>
       <a href='https://github.com/davidfeng88/CollisionViz' target="_blank">
