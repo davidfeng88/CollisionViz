a012b7ce135c119ce74e7e642b45feeae69aa6e8
davidfeng88 <david.feng.ge@gmail.com>
2017-08-23 20:12:22 -0400

    adjust modal padding

--- lib/javascript/components/header/about_collision_viz.jsx
+++ lib/javascript/components/header/about_collision_viz.jsx
@@ -6,7 +6,7 @@ const AboutCollisionViz = props => (
   <ModalTemplate {...props}>
     <h3>About CollisionViz</h3>
     <p className='modal-row'>
-      CollisionViz is a data visualization web app 
+      CollisionViz is a data visualization web app
       for motor vehicle collisions in New York City.
     </p>
     <p className='modal-row'>
@@ -15,7 +15,7 @@ const AboutCollisionViz = props => (
     <p className='modal-row'>
       2. Start/pause the visualization. Click on markers for collision details.
     </p>
-    <div className='modal-link-div'>
+    <div className='flex-row'>
       <a href='https://github.com/davidfeng88/CollisionViz' target='_blank'>
         GitHub Repo
       </a>
